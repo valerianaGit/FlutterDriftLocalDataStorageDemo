1aaab3298b76a85755ffeda2a63a739703ead9d7
vdr <valeria.do@gmail.com>
2023-04-17 22:49:40 -0500

    create home page scaffold

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,15 +10,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-    // TODO: SCAFFOLD 
+    return Scaffold(
+body: 
+Column(
+ children: [
+  // TEXTFIELD to enter todo item
+TextField(
+  textCapitalization: TextCapitalization.words,
+  onTap:() {
+    //
+  },
+),
+    // CHECK DATABASE BUTTON 
+    // DELETE this button for deployment
+                ElevatedButton(
+                  child: const Text('Check database'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => DriftDbViewer(database)
+                        )
+                        );
+                    
+                  },
+                ),
 
-    //TODO: BODY 
-    //TODO: TEXTFIELD 
-
-    //TODO: CHECK DATABASE BUTTON 
-
-    //TODO: LISTVIEW
+    // LISTVIEW - 
+    Expanded(
+  child:   ListView(
+    scrollDirection: Axis.vertical,
+  //TODO: Add dummy pictures for this basic suite 
+  ),
+),
     //TODO: FLOATING ACTION BUTTON IN SCAFFOLD  
+
+ ],
+  
+
+),
+
+    );
+
   }
 }
