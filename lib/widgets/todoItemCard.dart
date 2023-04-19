324bfb7e187f95f194a2e862cbf9d0389a76b520
vdr <valeria.do@gmail.com>
2023-04-19 18:03:53 -0500

    create todo item card widget
    widget to represent each todo item

---
+++ lib/widgets/todoItemCard.dart
@@ -0,0 +1,29 @@
+// Card widet that will be used in the todolist list view to represent each todo item in the list
+import 'package:flutter/material.dart';
+import 'package:demo_drift_package/constants/constants.dart';
+
+class TodoItemCard extends StatelessWidget {
+  final String content;
+
+  const TodoItemCard({super.key, required this.content});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 5.0,
+          color: kdarkColor,
+          child: Container(
+            alignment: Alignment.center,
+            child: Text(content,
+            style: kTodoItemCardStyle,
+              ),
+          ),
+        ),
+        ),
+    );
+  }
+}
