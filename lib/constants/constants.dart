d6697b8c7afe28c4ee06851a5168050ef6017fd3
vdr <valeria.do@gmail.com>
2023-04-20 23:05:21 -0500

    update constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 var kCursorColor = Colors.green[900];
 var kdarkColor = Colors.green;
-var kTodoItemCardStyle = const TextStyle(fontSize: 20.0, color: Colors.white);
+var kTodoItemCardStyle = const TextStyle(fontSize: 20.0, color: Colors.black, backgroundColor: Colors.amber);
