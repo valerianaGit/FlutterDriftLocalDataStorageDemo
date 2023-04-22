fe823c006b2c59f4191d19adc528e9a99d998849
vdr <valeria.do@gmail.com>
2023-04-22 15:37:16 -0500

    update device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'constants/strings.dart';
 import 'screens/home_page.dart';
+import 'package:flutter/services.dart'; // necessary for device orientation 
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+         SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
     return MaterialApp(
       title: kAppTitle,
       theme: ThemeData(
