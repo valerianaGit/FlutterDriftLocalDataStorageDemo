e996399eca35f073f80c05c051a965dd948ea7a3
vdr <valeria.do@gmail.com>
2023-04-22 16:56:13 -0500

    create Riverpod at root

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/material.dart';
 import 'constants/strings.dart';
 import 'screens/home_page.dart';
-import 'package:flutter/services.dart'; // necessary for device orientation 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'data/drift_db.dart';
+import 'package:flutter/services.dart'; // necessary for device orientation
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      //Enable riverpod for the entire application
+      const ProviderScope(child: MyApp()));
 }
+
+// create provider we will be able to use everywhere on app
+//TODO: TUTORIAL on several different providers on same app, not this one though, not a riverpod tutorial
+final todoDBProvider = Provider.autoDispose((ref) => TodoDatabase()); // avoid unnecessary cost and restore state when provider is no longer in use
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-         SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: kAppTitle,
       theme: ThemeData(
-        primarySwatch: Colors.green, // TODO: CREATE A CONSTANT , EXPORT THEME CODE INTO A CONSTANT 
+        primarySwatch: Colors
+            .green, // TODO: CREATE A CONSTANT , EXPORT THEME CODE INTO A CONSTANT
       ),
       home: const HomePage(),
     );
+    // Check your state management tool for lifecycles (i.e. initialized / alive/ paused/ disposed)
+    //dispose: (context, db) => db.close(),
+    // dispose is a method from Provider,
+    // Riverpod has autodispose, used appended in the provider itself
+    //docs -> https://riverpod.dev/docs/concepts/modifiers/auto_dispose
   }
 }
