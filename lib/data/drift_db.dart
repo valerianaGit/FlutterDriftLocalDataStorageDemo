5e5eca1ce15d02c1eac5ea1a42d74caa65637e28
vdr <valeria.do@gmail.com>
2023-04-21 19:00:30 -0500

    created drift database

---
+++ lib/data/drift_db.dart
@@ -0,0 +1,54 @@
+//STEP 1 - IMPORT drift package
+
+import 'package:drift/drift.dart';
+import 'dart:io'; // needed to generate/find a path to db
+import 'package:drift/native.dart'; // needed to use NativeDatabase
+import 'package:path_provider/path_provider.dart'; // needed to get path to document's directory where application can place user generated data
+import 'package:path/path.dart'
+    as p; // needed to generate/find a path to db -> p is the name we will use in our code to join the path
+
+// import generated file by drift
+part 'drift_db.g.dart';
+
+//STEP 2 - Create data model table
+
+class Todos extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get content => text()
+      .nullable()(); //TODO: Share tutorial with table with default values and store date, boolean values etc.
+}
+
+//STEP 3 - Create Drift database from table
+
+@DriftDatabase(tables: [Todos])
+class TodoDatabase extends _$TodoDatabase {
+//STEP 4 - Open connection to database in system
+
+  TodoDatabase() : super(_openConnection());
+  @override
+  int get schemaVersion =>
+      1; // strongly recomend this, to be able to update to new versions of your schema, for any future changes
+
+//STEP 5 - Run command [ flutter pub run build_runner build ] to generate .g file and create queries
+  //QUERIES
+  //CREATE
+  Future insertNewTodo(Todo todo) => into(todos).insert(todo);
+   //Create using post companion -  to create post since id is an autoincrement and cannot be entered manually
+   
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory(); //
+    final file = File(p.join(dbFolder.path,
+        'tododb.sqlite')); // create File object for our database, named tododb.sqlite
+    return NativeDatabase(file,
+        logStatements: true); // SET logs to true, for easier development
+  });
+  //GOTCHA! -> using Provider package, we would need to dispose, close db on main.dart. Dispose doesn't seem to be necessary using Riverpod
+}
+
+//TODO: CREATE A VISUAL COMMENT HERE  OF WHAT THE TABLE WILL LOOK LIKE
