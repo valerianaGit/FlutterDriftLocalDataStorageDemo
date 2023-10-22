780e82f89cf082aa81994f4ebca6fc0304fdb9a2
vdr <valeria.do@gmail.com>
2023-10-21 19:03:27 -0500

    create app theme and update constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
-var kCursorColor = Colors.teal[900];
-var kdarkColor = Colors.teal;
+//var kCursorColor = Colors.teal[900];
+var kdarkColor = Color(0xff171d49);
 var kTodoItemCardStyle = const TextStyle(fontSize: 20.0, color: Colors.white);
+
+class AppThemes {
+  final ThemeData lightTheme = ThemeData(
+    primaryColor: Color(0xff171d49),//Colors.blue,
+    appBarTheme: AppBarTheme(
+      color: Color(0xff171d49),
+    ),
+    textSelectionTheme: TextSelectionThemeData(
+      selectionColor: Colors.grey,
+      cursorColor: Color(0xff171d49),
+      selectionHandleColor: Color(0xff005e91),
+    ),
+    //backgroundColor: Colors.white,
+    brightness: Brightness.light,
+    highlightColor: Colors.white,
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: Color(0xff171d49),//Colors.blue,
+        focusColor: Colors.blueAccent,
+        splashColor: Colors.lightBlue),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+  );
+
+  ThemeData halloweenTheme = ThemeData(
+      colorScheme: ColorScheme.fromSwatch(
+    primarySwatch: Colors.orange,
+  ));
+}
