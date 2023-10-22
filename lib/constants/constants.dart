cd2fcabb75ffb96e3efb55275735db9cbd889482
vdr <valeria.do@gmail.com>
2023-10-21 19:46:25 -0500

    delete unnecessary code

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-//var kCursorColor = Colors.teal[900];
 var kdarkColor = const Color(0xff171d49);
 var kTodoItemCardStyle = const TextStyle(fontSize: 20.0, color: Colors.white);
 
